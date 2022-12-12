a6e4db6d6d1a236ecf761d53b5b234cd56f44e82
Toscanelli Mattia <mattia.toscanelli@student.supsi.ch>
2022-12-12 09:39:02 +0100

    Fixed Homepage

---
+++ src/js/main.js
@@ -0,0 +1,17 @@
+(function ($) {
+    // VERTICALLY ALIGN FUNCTION
+    $.fn.vAlign = function() {
+        return this.each(function(i){
+            var ah = $(this).height();
+            var ph = $(this).parent().height();
+            var mh = Math.ceil((ph-ah) / 2) - 16;
+            $(this).css('margin-top', mh);
+        });
+   };
+})(jQuery);
+
+$('.card-body').vAlign();
+
+$( window ).resize(function() {
+    $('.card-body').vAlign();
+  });

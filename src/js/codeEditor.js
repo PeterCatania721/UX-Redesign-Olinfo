9826f0b144df5081adf199776dc235a68e297258
Catania Peter <peter.catania@student.supsi.ch>
2022-12-13 23:32:21 +0100

    add previous submissions table

--- src/js/codeEditor.js
+++ src/js/codeEditor.js
@@ -1,5 +1,4 @@
 var editor = CodeMirror.fromTextArea(document.getElementById("code"), {
-    value: "function myScript(){return 100;}\n",
     mode: "javascript",
     styleActiveLine: true,
     lineNumbers: true,
---
+++ src/js/problem.js
@@ -0,0 +1,1 @@
+var oTable = $('#previos-submissions').DataTable();

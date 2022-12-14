6f7d545a5635e8f03975852e49865db1e13e6012
Catania Peter <peter.catania@student.supsi.ch>
2022-12-14 15:29:37 +0100

    improved data table

--- src/js/codeEditor.js
+++ src/js/codeEditor.js
@@ -2,7 +2,7 @@ var editor = CodeMirror.fromTextArea(document.getElementById("code"), {
     mode: "javascript",
     styleActiveLine: true,
     lineNumbers: true,
-    lineWrapping: true
+    lineWrapping: true,
 });
 
 editor.getDoc().setValue('Write your code here!!\n\n\n\n\n\n\n\n\n\n\n\n\n\n');
--- src/js/problem.js
+++ src/js/problem.js
@@ -1,1 +1,14 @@
-var oTable = $('#previos-submissions').DataTable();
+var oTable = $('#previos-submissions').DataTable( {
+    dom: '<"top">rt<"bottom"lp><"clear">',
+    "language": {
+        "lengthMenu": "Display _MENU_ submissions",
+    }
+});
+
+$(window).on('load', function() {
+    $('.bottom').addClass('row');
+    $('.bottom').children().addClass('col-md-6');
+    $('#previos-submissions_filter').children().css('font-size', '20px');
+    $('#previos-submissions_length').children().css('font-size', '18px');
+    $('#previos-submissions_length').css('margin', '2px 0 2px 0');
+});
